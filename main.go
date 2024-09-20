5d5824a4fe3cbcb56a8e0404483106723f220470
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-20 13:17:53 -0400

    feat(lsp/): Add base initialize lsp response and request message handling

--- main.go
+++ main.go
@@ -2,24 +2,63 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/json"
+	"log"
 	"os"
 
+	"github.com/ZeroBl21/go-lsp/lsp"
 	"github.com/ZeroBl21/go-lsp/rpc"
 )
 
 func main() {
-	fmt.Println("Hello there")
+	logger := getLogger("/home/zero/code/go/go-lsp/log.txt")
+	logger.Printf("I started")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(msg)
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("Got an error: %s", err)
+			continue
+		}
+
+		handleMessage(logger, method, contents)
 	}
 }
 
-func handleMessage(msg string) {
-	panic("unimplemented")
+func handleMessage(logger *log.Logger, method string, contents []byte) {
+	logger.Printf("Received msg with method: %s", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("Hey, we could't parse this: %s", err)
+		}
+		logger.Printf(
+			"Connected to: %s %s",
+			request.Params.ClientInfo.Name,
+			request.Params.ClientInfo.Version,
+		)
+
+		msg := lsp.NewInitializeResponse(request.ID)
+		reply := rpc.EncodeMessage(msg)
+
+		writer := os.Stdout
+		writer.Write([]byte(reply))
+
+		logger.Printf("Send the reply")
+	}
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("hey, you didn't give me a good file")
+	}
+
+	return log.New(logfile, "[ZLsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
